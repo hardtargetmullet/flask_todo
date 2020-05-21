a3fd6aaa30a74d6d778ec1d045fa9872cceeca56
Abe <elmeromero.an@gmail.com>
2020-05-20 17:17:56 -0700

    Added migrations scripts

--- app.py
+++ app.py
@@ -7,20 +7,22 @@ from flask import request
 from flask import redirect
 from flask import url_for
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://postgres:password@localhost:5432/todoapp'
 db = SQLAlchemy(app)
 
+migrate = Migrate(app, db)
+
 class Todo(db.Model):
     __tablename__ = 'todos'
     id = db.Column(db.Integer, primary_key=True)
     description = db.Column(db.String(), nullable=False)
+    completed = db.Column(db.Boolean, nullable=False, default=False)
 
     def __repr__(self):
         return f'<Todo - id: {self.id}, description: {self.description} >'
-
-db.create_all()
 
 @app.route('/')
 def index():
@@ -32,7 +34,7 @@ def create():
     body = {}
     try:
         text_str = request.get_json()['description']
-        task = Todo(description=text_str)
+        task = Todo(description2=text_str)
         db.session.add(task)
         db.session.commit()
         body['description'] = task.description
